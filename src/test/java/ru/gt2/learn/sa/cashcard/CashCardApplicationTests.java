e01cfa05afd2cb0e3a450f2c9f53df34a227f11c
rage <isakovich@pay.travel>
2023-07-17 12:02:30 +0300

    Created by spring initializr.

---
+++ src/test/java/ru/gt2/learn/sa/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gt2.learn.sa.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
